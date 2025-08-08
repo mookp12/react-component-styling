7545dd1d9e0c4aaea8cdc1b1ad0de4b70c340e24
mookp12 <kraiviwat.p@gmail.com>
2025-08-08 16:26:49 +0700

    feat: add Button and Alert components with styles and icons

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,3 +1,81 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import FrownIcon from "../assets/frown.svg";
+import AlertCircleIcon from "../assets/alert-triangle.svg";
+import AlertTriangleIcon from "../assets/alert-circle.svg";
+import CheckCircleIcon from "../assets/check-circle.svg";
 
-function Alert() {}
+function Alert(props) {
+  const types = {
+    error: {
+        color: "#F9C8C8",
+        icon: FrownIcon,
+        iconColor: "#ED5050",
+        text: "This is error alert box"
+    },
+    warning: {
+        color: "#F9D9C8",
+        icon: AlertTriangleIcon,
+        iconColor: "#EA712D",
+        text: "This is warning alert box"
+    },
+    info: {
+        color: "#F9EBC8",
+        icon: AlertCircleIcon,
+        iconColor: "#F29811",
+        text: "This is info alert box"
+    }, 
+    success: {
+        color: "#CEF7CD",
+        icon: CheckCircleIcon,
+        iconColor: "#14944F",
+        text: "This is success alert box"
+    }
+  };
+
+  const alertType = types[props.type] || types.error;
+
+  return (
+    <section
+      css={css`
+        width: 650px;
+        height: 76px;
+        border-radius: 10px;
+        padding-left: 16px;
+        padding-right: 16px;
+        gap: 8px;
+
+        background-color: ${alertType.color};
+        opacity: 1;
+
+        display: flex;
+        align-items: center;
+        justify-content: start;
+        position: relative;
+        top: 0;
+        left: 0;
+
+        font-family: 'Kanit', sans-serif;
+        font-weight: 700;
+        font-style: bold;
+        font-size: 20px;
+        line-height: 100%;
+        letter-spacing: 0px;
+        color: #444444;
+        opacity: 1;
+      `}
+      aria-label={`${alertType.text} alert`}
+    >
+      <img src={alertType.icon} alt={`${alertType.text} icon`} css={css`
+        width: 32px;
+        height: 32px;
+        color: ${alertType.iconColor};
+        opacity: 1;
+      `}    />
+      <span>{alertType.text}</span>
+    </section>
+  );
+}
+
+export default Alert;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,47 @@
 // Start coding here
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
 
-function Button() {}
+function Button(props) {
+  const colors = {
+    primary: "#074EE8",
+    secondary: "#07A4E8"
+  };
+
+  const backgroundColor = colors[props.color] || colors.primary;
+
+  return (
+    <button
+      css={css`
+        width: 171.19px;
+        height: 50px;
+        border-radius: 4px;
+        padding-left: 16px;
+        padding-right: 16px;
+        gap: 8px;
+        
+        background-color: ${backgroundColor};
+        border: none;
+        cursor: pointer;
+        opacity: 1;
+        
+          font-family: 'Sarabun', sans-serif;
+          font-size: 16px;
+          font-weight: 400;
+          color: white;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        
+        &:hover {
+          opacity: 0.9;
+        }
+      `}
+    >
+      {props.text}
+    </button>
+  );
+}
+
+export default Button;
